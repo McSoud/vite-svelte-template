6caa40b99a93e2b19d1b4b60be84cf0590ea282c
mcsoud <firas.maksoud@hotmail.com>
2025-04-11 18:56:38 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-routing";
+  import Header from "./lib/components/template/Header.svelte";
+  import PageHome from "@/lib/pages/home/_index.svelte";
+  import Footer from "./lib/components/template/Footer.svelte";
+</script>
+
+<Router url="">
+  <Header />
+  <Route path="/"><PageHome /></Route>
+  <Footer />
+</Router>
---
+++ src/lib/pages/home/_index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Main from "@/lib/components/template/Main.svelte";
+</script>
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+<Main id="home" class="container bg-red-500">Test</Main>
